c6bb33ea38bdce0bd1d92e268e1e8063da3e9ad5
RuslanHamidov <rhamidov202@gmail.com>
2024-11-02 13:55:48 +0400

    change routes to activerecord queries

--- blog-backend/routes/blog_routes.rb
+++ blog-backend/routes/blog_routes.rb
@@ -2,21 +2,6 @@ require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/activerecord'
 
-db_example = [{id: 1, title: "sample title", body: "this is body of first post"},
-              {id: 2, title: "title2", body: "this is body of second post"},
-              {id: 3, title: "title2", body: "this is body of second post"},
-              {id: 4, title: "title2", body: "this is body of second post"},
-              {id: 5, title: "title2", body: "this is body of second post"},
-              {id: 6, title: "title2", body: "this is body of second post"},
-              {id: 7, title: "title2", body: "this is body of second post"},
-              {id: 8, title: "title2", body: "this is body of second post"}
-              ]
-
-#get request body
-def getBody (req)
-    req.body.rewind
-    return JSON.parse(req.body.read)
-end
 
 #ensure content_type is json
 before do
@@ -35,33 +20,32 @@ end
 
 #show all posts
 get '/posts' do
-  json db_example
+  json Post.all
 end
 
 #get particular post
 get '/posts/:id' do
-  id = params["id"].to_i
-  json db_example[id]
+  post = Post.find(params[:id])
+  json post
 end
 
 #make new post
 post '/posts' do
-  data = getBody(request)
-  new_post = {id: db_example.length + 1,title: data["title"], body: data["body"]}
-  db_example.push(new_post)
-  json new_post
+  post = Post.create(title: params[:title], body: params[:body])
+  json post
 end
 
 #edit post
 put '/posts/:id' do
-  id = params["id"].to_i
-  body = getBody(request)
-  db_example[id - 1][:title] = body["title"]
-  db_example[id - 1][:body] = body["body"]
+  post = Post.find(params[:id])
+  post.update(title: params[:title], body: params[:body])
+  post.save
+  redirect '/posts/' + params[:id]
 end
 
 #delete particular post
 delete '/posts/:id' do
-  id = (params["id"].to_i)
-  db_example.delete_at(id)
+  post = Post.find(params[:id])
+  post.destroy
+  redirect '/posts'
 end
